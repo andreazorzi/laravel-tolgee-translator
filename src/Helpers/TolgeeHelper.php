0b05f4c5a20438837bf72e6aedb4e6012b95672a
Andrea Zorzi <andreaz@europlan.it>
2025-03-24 12:13:03 +0100

    Change helper html and behavior
    Update on click behavior in order to still open links

--- src/Helpers/TolgeeHelper.php
+++ src/Helpers/TolgeeHelper.php
@@ -25,46 +25,42 @@ if (!function_exists('tolgee')) {
         }
         
         return '
-            <a id="tolgee-'.Str::slug($key).'" href="'.Tolgee::get_translation_link($key).'" target="_blank" class="tolgee-link">
-                <span title="'.$key.'">
-                    '.$translation.'
-                    
-                    <img src="https://docs.tolgee.io/img/tolgeeLogo.svg" />
-                </span>
-            </a>
+            <span id="tolgee-'.Str::slug($key).'" title="Alt + Click to open this translation on Tolgee" onclick="if(event.altKey){ window.open(\''.Tolgee::get_translation_link($key).'\', \'_blank\'); return false; }">
+                '.$translation.'
+                
+                <img src="https://docs.tolgee.io/img/tolgeeLogo.svg" />
+            </span>
             <style>
                 #tolgee-'.Str::slug($key).'{
                     color: inherit;
                     text-decoration: none;
-                
-                    & span{
-                        display: inline-block;
-                        padding: 3px 10px;
-                        margin-top: -4px;
-                        margin-left: -10px;
-                        border: 1px solid transparent;
-                        
-                        border-radius: 10px;
-                        transition: all 0.3s;
-                        -webkit-transition: all 0.3s;
-                        -moz-transition: all 0.3s;
-                        -o-transition: all 0.3s;
-                        
-                        --color: #ec407a;
-                        
-                        &:hover{
-                            border: 1px solid var(--color);
-                            background-color: color-mix(in srgb, var(--color), transparent 95%);
-                            
-                            & img{
-                                display: inline-block !important;
-                            }
-                        }
+                    display: inline-block;
+                    padding: 3px 10px;
+                    margin-top: -4px;
+                    margin-left: -10px;
+                    border: 1px solid transparent;
+                    cursor: pointer;
+                    
+                    border-radius: 10px;
+                    transition: all 0.3s;
+                    -webkit-transition: all 0.3s;
+                    -moz-transition: all 0.3s;
+                    -o-transition: all 0.3s;
+                    
+                    --color: #ec407a;
+                    
+                    &:hover{
+                        border: 1px solid var(--color);
+                        background-color: color-mix(in srgb, var(--color), transparent 95%);
                         
                         & img{
-                            display: none;
-                            width: 20px;
+                            display: inline-block !important;
                         }
+                    }
+                    
+                    & img{
+                        display: none;
+                        width: 20px;
                     }
                 }
             </style>
